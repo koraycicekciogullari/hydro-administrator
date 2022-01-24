399c3ed6634cd52f00f7840d88a3ae285537282b
Koray Çiçekçioğulları <k.cicekciogullari@gmail.com>
2022-01-24 04:42:21 +0300

    Bugfix

--- src/Routes/administrator-route.php
+++ src/Routes/administrator-route.php
@@ -5,13 +5,15 @@ use Koraycicekciogullari\HydroAdministrator\Controllers\LoginController;
 use Koraycicekciogullari\HydroAdministrator\Controllers\LogoutController;
 use Koraycicekciogullari\HydroAdministrator\Controllers\PasswordController;
 use Koraycicekciogullari\HydroAdministrator\Controllers\ProfileController;
+use Koraycicekciogullari\HydroAdministrator\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', LoginController::class);
+Route::middleware('api')->post('api/login', LoginController::class);
 
 Route::middleware('auth:sanctum')->post('api/logout', LogoutController::class);
 
-Route::middleware(['auth:sanctum'])->prefix('api')->group(function(){
+Route::middleware(['auth:sanctum', 'api'])->prefix('api')->group(function(){
+    Route::apiResource('user', UserController::class)->only('index');
     Route::prefix('admin')->middleware('api')->group(function(){
         Route::apiResource('profile', ProfileController::class)->only('index', 'update');
         Route::apiResource('administrator', AdministratorController::class)->except('edit', 'create');
